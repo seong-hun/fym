8e0ffdb440c848d8dcc467caa000dbcc089302a8
jinraekim <kjl950403@gmail.com>
2019-08-01 14:36:12 +0900

    minor bug fix file name change

---
+++ main_missile.py
@@ -0,0 +1,42 @@
+import numpy as np
+import numpy.linalg as nla
+import time
+
+from nrfsim.envs.stationary_target_interception import StationaryTargetInterceptionEnv
+
+
+np.random.seed(1)
+
+env = StationaryTargetInterceptionEnv(
+        initial_state=np.array([-10e3, 0, 200, np.deg2rad(30)]).astype('float')
+        )
+
+time_step = 0.01
+t0 = 0      # launch time (no guidance loop)
+t1 = 1.5    # first boost phase
+t2 = 8.5    # second boost phase
+tf = 40     # final simulation time
+time_series = np.arange(t0, tf, time_step)
+
+obs = env.reset()
+# elpased time
+start_time = time.time()
+
+for i in time_series:
+    if i < t1:
+        controls = np.zeros(1) # should be zero until the first boost phase
+    else:
+        controls = np.zeros(1) # should be the output of controller
+
+    # Need logging here
+    next_obs, reward, done, _ = env.step(controls)
+    print(obs)
+
+    if done:
+        break
+
+    obs = next_obs
+
+elapsed_time = time.time() - start_time
+print('simulation time = ', tf - t0, '[s]')
+print('elpased time = ', elapsed_time, '[s]')
