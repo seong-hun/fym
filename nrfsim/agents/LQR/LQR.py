00158ba8d8d47aec3fc9b3bc0f8528fe6baf5a78
JungYT <skfmsgka@snu.ac.kr>
2019-08-13 17:07:15 +0900

    PEP8 correction
    PEP8 correction

--- nrfsim/agents/LQR/LQR.py
+++ nrfsim/agents/LQR/LQR.py
@@ -1,31 +1,35 @@
 import numpy as np
 import scipy.linalg as lin
 import numpy.linalg as nla
-#from nrfsim.~ import Linearization
 
-class LQR():
+
+# from nrfsim.~ import Linearization
+
+
+class LQR:
     def __init__(self):
         pass
 
-    def clqr(self, A: np.array, B: np.array, Q: np.array, R: np.array) \
-        -> np.array:
-        X = lin.solve_continuous_are(A, B, Q, R)
-        if np.size(R) == 1:
-            K = (np.transpose(B).dot(X))/R
+    def clqr(self, a: np.array, b: np.array, q: np.array, r: np.array) \
+            -> np.array:
+        x = lin.solve_continuous_are(a, b, q, r)
+        if np.size(r) == 1:
+            k = (np.transpose(b).dot(x)) / r
         else:
-            K = nla.inv(R).dot((np.transpose(B).dot(X)))
-        eig_vals, eig_vecs = nla.eig(A-B.dot(K))
-        return K, X, eig_vals, eig_vecs
+            k = nla.inv(r).dot((np.transpose(b).dot(x)))
+        eig_vals, eig_vecs = nla.eig(a - b.dot(k))
+        return k, x, eig_vals, eig_vecs
 
-    def dlqr(self, A: np.array, B: np.array, Q: np.array, R: np.array) \
-        -> np.array:
-        X = lin.solve_discrete_are(A, B, Q, R)
-        if np.size(R) == 1:
-            K = (np.transpose(B).dot(X))/R
+    def dlqr(self, a: np.array, b: np.array, q: np.array, r: np.array) \
+            -> np.array:
+        x = lin.solve_discrete_are(a, b, q, r)
+        if np.size(r) == 1:
+            k = (np.transpose(b).dot(x)) / r
         else:
-            K = nla.inv(R).dot((np.transpose(B).dot(X)))
-        eig_vals, eig_vecs = nla.eig(A-B.dot(K))
-        return K, X, eig_vals, eig_vecs
+            k = nla.inv(r).dot((np.transpose(b).dot(x)))
+        eig_vals, eig_vecs = nla.eig(a - b.dot(k))
+        return k, x, eig_vals, eig_vecs
+
 
 '''
     def nlqr(f, g, Q: array, R: array) -> array, array, array, array:
