1cb44694f2c532252031f04fdc88f80bfa9fae9e
JungYT <skfmsgka@snu.ac.kr>
2019-08-22 15:38:32 +0900

    Simplify LQR module (#61)
    Extract LQR.py file from LQR folder.
    Modify LQR module from class to function.
    LQR module has not to be class module.

---
+++ main_quadrotor_LQR.py
@@ -0,0 +1,7 @@
+import numpy as np
+from nrfsim.utils.linearization import jacob_analytic
+from nrfsim.utils.linearization import jacob_numerical
+from nrfsim.agents.LQR.LQR import LQR
+from nrfsim.models.quadrotor import Quadrotor
+
+init = []
--- main_test_LQR.py
+++ main_test_LQR.py
@@ -1,12 +1,11 @@
 import numpy as np
-from nrfsim.agents.LQR.LQR import LQR
+from nrfsim.agents.LQR import clqr, dlqr
 
 A = np.array([[1, 2], [2, -1]])
 B = np.array([[1], [2]])
 Q = np.eye(2)
 R = 1
 
-lqr = LQR()
 
-K_con, X_con, eig_vals_con, eig_vecs_con = lqr.clqr(A, B, Q, R)
-K_dis, X_dis, eig_vals_dis, eig_vecs_dis = lqr.dlqr(A, B, Q, R)
+K_con, X_con, eig_vals_con, eig_vecs_con = clqr(A, B, Q, R)
+K_dis, X_dis, eig_vals_dis, eig_vecs_dis = dlqr(A, B, Q, R)
