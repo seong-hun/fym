0bf37c1e65df3eb423e1fb959df63a076e29200e
Seong-hun Kim <kshoon92@gmail.com>
2019-10-18 17:45:16 +0900

    Modify setup.py and README.md

--- README.md
+++ README.md
@@ -1,16 +1,7 @@
 # Aerodynamic Simulator for Testing Deep RL Algorithms
 
-[nrf-main](https://github.com/seong-hun/nrf-main)
-
 ## Installation
 
-- Clone the repo and change the directory to it
-  ```
-	git clone https://github.com/fdcl-nrf/fym.git
-	cd fym
-	```
-
-- Install the fym package
-	```
-	pip install -e .
-	```
+```python
+pip install fym
+```
--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from setuptools import setup
 
 setup(
     name='fym',
-    version='0.2.1',
-    install_requires=['gym', 'matplotlib', 'numdifftools']
+    version='0.2.3',
+    description='SNU FDCL Fym: A gym for flight dynamics',
+    author='SNU FDCL',
+    url='https://github.com/fdcl-nrf/fym',
+    license='',
+    install_requires=['gym', 'matplotlib', 'numdifftools'],
+    packages=['fym']
 )
