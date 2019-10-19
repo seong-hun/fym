7129d3d6dfc927c720aa1233a16f23d3902e35a6
Seong-hun Kim <kshoon92@gmail.com>
2019-10-19 20:24:08 +0900

    Update the setup.py version
    - v0.2.1 -> v0.3.0

--- setup.py
+++ setup.py
@@ -9,11 +9,13 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='fym',
-    version='0.2.1',
+    version='0.3.0',
     url='https://github.com/fdcl-nrf/fym',
-    description='Flight simulator for various purpose',
+    author='SNU FDCL',
+    description='SNU FDCL Fym: Flight simulator for various purpose',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    license='',
     include_package_data=True,
     packages=find_packages(),
     install_requires=['gym', 'matplotlib', 'numdifftools']
