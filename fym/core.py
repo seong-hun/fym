fd14ed9eca2d80655d693a91ed7f7df5c925a6d9
Seong-hun Kim <kshoon92@gmail.com>
2019-10-19 16:16:30 +0900

    Improve core (#100)
    * Improve the core.py
    
    - Infer the `observation_space` based on `systems`.
    - Remove the `obs_space` and `obs_space` arguments in `__init__` method,
      and replace it with attribute checking statements.
    - Integration improved. Now the integrator uses
      `scipy.integrate.odeint` (#84).
    - Add Clock class which helps us manage the iteration time, episode
      number, etc, more intuitively and clearly.
    - Remove the need of `BaseSystem.deriv` and replace it with
      `BaseEnv.derivs`. This is more clear when we design the environment
      of which systems are interacting in very complex way.
    
    * Add packing and unpacking functions
    
    - State dictionary can now be flattened with unpacking function
    - State list of numpy arrays can now be packed with packing function

--- fym/core.py
+++ fym/core.py
@@ -1,163 +1,164 @@
 from collections import OrderedDict
+from itertools import chain
+import functools
 import numpy as np
+from scipy.integrate import odeint
 import gym
 
 
+def deep_flatten(arg):
+    if arg == []:
+        return arg
+    if isinstance(arg, (list, tuple)):
+        return deep_flatten(arg[0]) + deep_flatten(arg[1:])
+    elif isinstance(arg, (np.ndarray, float, int)):
+        return [np.asarray(arg).ravel()]
+
+
+def flatten(arglist):
+    return [np.asarray(arg).ravel() for arg in arglist]
+
+
+def infinite_box(shape):
+    return gym.spaces.Box(-np.inf, np.inf, shape=shape, dtype=np.float32)
+
+
+def infer_obs_space(systems):
+    """
+    Infer the gym observation space from the ordered dictionary ``systems``,
+    and return a gym.spaces.Dict
+    """
+    obs_space = gym.spaces.Dict(
+        {k: infinite_box(s.state_shape) for k, s in systems.items()})
+    return obs_space
+
+
 class BaseEnv(gym.Env):
-    def __init__(self, systems: list, dt: float,
-                 obs_sp: gym.spaces.Space, act_sp: gym.spaces.Space,
-                 rk4_steps: int=2):
+    def __init__(self, systems: list, dt: float, infer_obs_space=True,
+                 odeint_option={}):
         self.systems = OrderedDict({s.name: s for s in systems})
-        self.state_index = np.cumsum([system.state_size for system in systems])
-        self.control_index = np.cumsum([system.control_size for system in systems])
+
+        if infer_obs_space and not hasattr(self, 'observation_space'):
+            self.observation_space = self.infer_obs_space(self.systems)
+
+        # Indices for packing
+        self.state_index = [system.state_shape for system in systems]
 
         # Necessary properties for gym.Env
-        self.observation_space = obs_sp
-        self.action_space = act_sp
+        if not hasattr(self, 'observation_space'):
+            raise NotImplementedError('The observation_space is not defined.')
 
-        self.rk4_steps = rk4_steps
-        self.dt = dt
+        if not hasattr(self, 'action_space'):
+            raise NotImplementedError('The action_space is not defined.')
+
+        self.clock = Clock(dt=dt)
+
+        self.odeint_option = odeint_option
 
     def reset(self):
         initial_states = {
             k: system.reset() for k, system in self.systems.items()
         }
         self.states = initial_states
-        self.clock = 0
-        return self.get_ob()
+        self.clock.reset()
+        return self.observation(self.states)
 
-    def step(self, controls):
-        xs = np.hstack(list(self.states.values()))
-        us = np.hstack(list(controls.values()))
-        t = self.clock
+    def get_next_states(self, t, states, action):
+        xs = self.unpack_state(states)
+        t_span = [t, t + self.clock.dt]
 
-        nxs = rk4(
-            self.derivs,
-            xs,
-            t + np.linspace(0, self.dt, self.rk4_steps), us
-        )
+        func = self.ode_wrapper(self.derivs)
+        nxs = odeint(func, xs, t_span, args=(action,), tfirst=True)
 
         nxs = nxs[-1]
-        next_states = self.resolve(nxs, self.state_index)
+        next_states = self.pack_state(nxs)
 
-        # Reward and terminal
+        return next_states
 
-        reward = self.get_reward(controls)
-        terminal = self.terminal()
+    def ode_wrapper(self, func):
+        @functools.wraps(func)
+        def wrapper(t, y, *args):
+            states = self.pack_state(y)
+            return func(t, states, *args)
+        return wrapper
 
-        # Update internal state and clock
-        self.states = next_states
-        self.clock = t + self.dt
-
-        return (self.get_ob(), reward, terminal, {})
-
-    def get_reward(self, controls: dict) -> float:
-        raise NotImplementedError("Reward function is not defined in the Env.")
-
-    def terminal(self) -> bool:
-        raise NotImplementedError("Terminal is not defined in the Env.")
-
-    def get_ob(self) -> np.ndarray:
-        raise NotImplementedError("Observation is not defined in the Env.")
-
-    def resolve(self, ss, index):
-        *ss, _ = np.split(ss, index)
-        some = OrderedDict(zip(self.systems.keys(), ss))
-        return some
-
-    def derivs(self, xs, t, us):
+    def derivs(self, t, states, action):
         """
-        Returns:
-            *xs*: ndarray
-                An array of aggregated states.
-            *t*: float
-                The time when the derivatives calculated.
-            *us*: ndarray
-                An array of aggregated control inputs.
-        """
-        states = self.resolve(xs, self.state_index)
-        controls = self.resolve(us, self.control_index)
+        It is recommened to override this method by a user-defined ``derivs``.
 
-        derivs = []
-        for s, x, u in zip(*map(dict.values, [self.systems, states, controls])):
-            derivs.append(s.deriv(x, t, u, s.external(states, controls)))
-
+        Sample:
+        ```python
+        controls = self.pack_action(us, self.control_index)
+        derivs = [system.deriv(t, states, controls) for system in self.systems]
         return np.hstack(derivs)
+        ```
+        """
+        raise NotImplementedError
+
+    def pack_state(self, flat_state):
+        packed = OrderedDict(
+            zip(self.systems.keys(), pack(flat_state, self.state_index)))
+        return packed
+
+    def unpack_state(self, states):
+        unpacked = flatten(states.values())
+        return np.hstack(unpacked)
+
+    def step(self, action):
+        raise NotImplementedError
 
 
 class BaseSystem:
     def __init__(self, name, initial_state, control_size=0, deriv=None):
         self.name = name
         self.initial_state = initial_state
-        self.state_size = len(initial_state)
+        self.state_shape = self.initial_state.shape
         self.control_size = control_size
+
         if callable(deriv):
             self.deriv = deriv
 
-    def deriv(self, state, t, control, external):
+    @property
+    def initial_state(self):
+        return self._initial_state
+
+    @initial_state.setter
+    def initial_state(self, val):
+        self._initial_state = np.asarray(val)
+
+    def deriv(self):
         raise NotImplementedError("deriv method is not defined in the system.")
 
     def reset(self):
         return self.initial_state
 
 
-def rk4(derivs, y0, t, *args, **kwargs):
+class Clock:
+    def __init__(self, dt, max_t=None):
+        self.dt = dt
+        self.max_t = max_t
+
+    def reset(self):
+        self.t = 0
+
+    def tick(self):
+        self.t += self.dt
+
+    def get(self):
+        return self.t
+
+
+def pack(flat_state, indices):
     """
-    Integrate 1D or ND system of ODEs using 4-th order Runge-Kutta.
-    This is a toy implementation which may be useful if you find
-    yourself stranded on a system w/o scipy.  Otherwise use
-    :func:`scipy.integrate`.
-    *y0*
-        initial state vector
-    *t*
-        sample times
-    *derivs*
-        returns the derivative of the system and has the
-        signature ``dy = derivs(yi, ti)``
-    *args*
-        additional arguments passed to the derivative function
-    *kwargs*
-        additional keyword arguments passed to the derivative function
-    Example 1 ::
-        ## 2D system
-        def derivs6(x,t):
-            d1 =  x[0] + 2*x[1]
-            d2 =  -3*x[0] + 4*x[1]
-            return (d1, d2)
-        dt = 0.0005
-        t = arange(0.0, 2.0, dt)
-        y0 = (1,2)
-        yout = rk4(derivs6, y0, t)
-    Example 2::
-        ## 1D system
-        alpha = 2
-        def derivs(x,t):
-            return -alpha*x + exp(-t)
-        y0 = 1
-        yout = rk4(derivs, y0, t)
-    If you have access to scipy, you should probably be using the
-    scipy.integrate tools rather than this function.
+    Pack states from the flattened state using ``indices``.
+    The ``indices`` is a list or a tuple which must have the equal length
+    to the ``flat_state``.
     """
+    div_points = [0] + np.cumsum(
+        [np.prod(index) for index in indices]).tolist()
 
-    try:
-        Ny = len(y0)
-    except TypeError:
-        yout = np.zeros((len(t),), np.float_)
-    else:
-        yout = np.zeros((len(t), Ny), np.float_)
-
-    yout[0] = y0
-
-    for i in np.arange(len(t) - 1):
-
-        thist = t[i]
-        dt = t[i + 1] - thist
-        dt2 = dt / 2.0
-        y0 = yout[i]
-
-        k1 = np.asarray(derivs(y0, thist, *args, **kwargs))
-        k2 = np.asarray(derivs(y0 + dt2 * k1, thist + dt2, *args, **kwargs))
-        k3 = np.asarray(derivs(y0 + dt2 * k2, thist + dt2, *args, **kwargs))
-        k4 = np.asarray(derivs(y0 + dt * k3, thist + dt, *args, **kwargs))
-        yout[i + 1] = y0 + dt / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4)
-    return yout
+    packed = []
+    for i in range(len(indices)):
+        packed.append(
+            flat_state[div_points[i]:div_points[i+1]].reshape(indices[i]))
+    return packed
