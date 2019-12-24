0a9d35882117ddf5b3e37518e6c05012162082d1
Seong-hun Kim <kshoon92@gmail.com>
2019-12-24 21:38:53 +0900

    Hotfix: setup.py

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ setup(
     license='',
     include_package_data=True,
     packages=find_packages(),
+    python_requires=">=3.7",
     install_requires=[
-        'python >= 3.7',
         'gym',
         'matplotlib',
         'numdifftools',
