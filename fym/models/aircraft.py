c3657d66cabd78c4439f750b8f5614cd91838b64
Seong-hun Kim <kshoon92@gmail.com>
2020-01-21 05:38:01 +0900

    Add a morphing aircraft (#126)
    * Add a rotation module (rot.py)
    
    - `quat2dcm`, `angle2quat`, `quat2angle`, `angle2dcm`
    - `velocity2polar`
    
    * Add a morphing plane model

--- fym/models/aircraft.py
+++ fym/models/aircraft.py
@@ -1,8 +1,15 @@
 import gym
 from gym import spaces
-import numpy as np
 
-from fym.core import BaseSystem
+import numpy as np
+import numpy.linalg as nla
+from numpy import cos, sin
+import scipy.linalg as sla
+import scipy.interpolate
+import scipy.optimize
+
+from fym.core import BaseEnv, BaseSystem
+from fym.utils.rot import quat2dcm, quat2angle, angle2quat
 
 
 class Aircraft3Dof(BaseSystem):
@@ -92,3 +99,223 @@ class F16LinearLateral(BaseSystem):
 
     def deriv(self, x, u):
         return self.A.dot(x) + self.B.dot(u)
+
+
+class MorphingPlane(BaseEnv):
+    g = 9.80665  # [m/s^2]
+    mass = 10  # [kg]
+    S = 0.84  # reference area (norminal planform area) [m^2]
+    # longitudinal reference length (nominal mean aerodynamic chord) [m]
+    cbar = 0.288
+    b = 3  # lateral reference length (nominal span) [m]
+    Tmax = 50  # maximum thrust [N]
+
+    control_limits = {
+        "delt": (0, 1),
+        "dele": np.deg2rad((-10, 10)),
+        "dela": (-0.5, 0.5),
+        "delr": (-0.5, 0.5),
+    }
+
+    coords = {
+        "eta1": np.linspace(0, 1, 3),  # eta1
+        "eta2": np.linspace(0, 1, 3),  # eta2
+        "dele": np.deg2rad(np.linspace(-10, 10, 3)),  # dele
+        "alpha": np.deg2rad(np.linspace(-10, 20, 61))  # alpha
+    }
+
+    polycoeffs = {
+        "CD": [0.03802,
+               [-0.0023543, 0.0113488, -0.00549877, 0.0437561],
+               [[0.0012769, -0.00220993, 1166.938, 672.113],
+                [0.00188837, 0.000115637, -203.85818, -149.4225],
+                [-1166.928, 203.8535, 0.1956192, -115.13404],
+                [-672.111624, 149.417, 115.76766, 0.994464]]],
+        "CL": [0.12816,
+               [0.13625538, 0.1110242, 1.148293, 6.0995634],
+               [[-0.147822776, 1.064541, 243.35532, -330.0270179],
+                [-1.13021511, -0.009309088, 166.28991, -146.8964467],
+                [-243.282881, -166.2709286, 0.071258483, 4480.53564],
+                [328.541707, 148.945785, -4480.67456545, -0.99765511]]],
+        "Cm": [0.09406144,
+               [-0.269902, 0.24346326, -7.46727, -2.7296],
+               [[0.35794703, -7.433699, 647.83725, -141.0390569],
+                [6.8532466, -0.0510021, 542.882121, -681.325],
+                [-647.723162, -542.8638, 0.76322739, 2187.33517],
+                [135.66547, 678.941, -2186.1196, 0.98880322]]]
+    }
+
+    J_template = np.array([
+        [9323306930.82, -2622499.75, 56222833.68],
+        [-2622499.75, 0, 395245.59],
+        [56222833.68, 395245.59, 105244200037]
+    ]) / 10**9 / 103.47649 * mass
+    J_yy_data = np.array([
+        [96180388451.54, 96468774320.55, 97352033548.31],
+        [96180388451.54, 96720172843.10, 98272328292.52],
+        [96180388451.54, 97077342563.70, 99566216309.81]
+    ]).T / 10**9 / 103.47649 * mass
+    J_yy = scipy.interpolate.interp2d(
+        coords["eta1"], coords["eta2"], J_yy_data
+    )
+
+    def __init__(self, velocity, omega, quaternion, position):
+        self.vel = BaseSystem(velocity, name="velocity")
+        self.omega = BaseSystem(omega, name="omega")
+        self.quat = BaseSystem(quaternion, name="quaternion")
+        self.pos = BaseSystem(position, name="position")
+        super().__init__(
+            systems_dict={
+                "velocity": self.vel,
+                "omega": self.omega,
+                "quaternion": self.quat,
+                "position": self.pos,
+            }
+        )
+
+    def J(self, eta1, eta2):
+        J_temp = self.J_template
+        J_temp[1, 1] = self.J_yy(eta1, eta2)
+        return J_temp
+
+    def _aero_base(self, name, *x):
+        # x = [eta1, eta2, dele, alp]
+        a0, a1, a2 = self.polycoeffs[name]
+        return a0 + np.dot(a1, x) + np.sum(x * np.dot(a2, x), axis=0)
+
+    def CD(self, eta1, eta2, dele, alp):
+        return self._aero_base("CD", eta1, eta2, dele, alp)
+
+    def CL(self, eta1, eta2, dele, alp):
+        return self._aero_base("CL", eta1, eta2, dele, alp)
+
+    def Cm(self, eta1, eta2, dele, alp):
+        return self._aero_base("Cm", eta1, eta2, dele, alp)
+
+    def set_dot(self, x, u, eta):
+        v, omega, q, p = self.observe_list(x)
+        F, M = self.aerodyn(v, q, p, u, eta)
+        J = self.J(eta[0], eta[1])
+
+        # force equation
+        self.systems_dict["velocity"].dot = F / self.mass - np.cross(omega, v)
+
+        # moment equation
+        self.systems_dict["omega"].dot = (
+            nla.inv(J).dot(M - np.cross(omega, J.dot(omega)))
+        )
+
+        # kinematic equation
+        self.systems_dict["quaternion"].dot = 0.5 * np.append(
+            -omega.dot(q[1:]),
+            omega*q[0] - np.cross(omega, q[1:])
+        )
+
+        # navigation equation
+        self.systems_dict["position"].dot = quat2dcm(q).T.dot(v)
+
+    def state_readable(self, v=None, omega=None, q=None, p=None, preset="vel"):
+        VT = sla.norm(v)
+        alpha = np.arctan2(v[2], v[0])
+        beta = np.arcsin(v[1] / VT)
+
+        if preset == "vel":
+            return VT, alpha, beta
+        else:
+            _, theta, _ = quat2angle(q)
+            gamma = theta - alpha
+            Q = omega[1]
+            return {'VT': VT, 'gamma': gamma, 'alpha': alpha, 'Q': Q,
+                    'theta': theta, 'beta': beta}
+
+    def aerocoeff(self, *args):
+        # *args: eta1, eta2, dele, alp
+        # output: CL, CD, Cm, CC, Cl, Cn
+        return self.CL(*args), self.CD(*args), self.Cm(*args), 0, 0, 0
+
+    def aerodyn(self, v, q, p, u, eta):
+        delt, dele, dela, delr = u
+        x_cg, z_cg = 0, 0
+
+        VT, alp, bet = self.state_readable(v=v, preset="vel")
+        qbar = 0.5 * rho(-p[2]) * VT**2
+
+        CL, CD, Cm, CC, Cl, Cn = self.aerocoeff(*eta, dele, alp)
+
+        CX = cos(alp)*cos(bet)*(-CD) - cos(alp)*sin(bet)*(-CC) - sin(alp)*(-CL)
+        CY = sin(bet)*(-CD) + cos(bet)*(-CC) + 0*(-CL)
+        CZ = cos(bet)*sin(alp)*(-CD) - sin(alp)*sin(bet)*(-CC) + cos(alp)*(-CL)
+
+        S, cbar, b, Tmax = self.S, self.cbar, self.b, self.Tmax
+
+        X_A = qbar*CX*S  # aerodynamic force along body x-axis
+        Y_A = qbar*CY*S  # aerodynamic force along body y-axis
+        Z_A = qbar*CZ*S  # aerodynamic force along body z-axis
+
+        # Aerodynamic moment
+        l_A = qbar*S*b*Cl + z_cg*Y_A  # w.r.t. body x-axis
+        m_A = qbar*S*cbar*Cm + x_cg*Z_A - z_cg*X_A  # w.r.t. body y-axis
+        n_A = qbar*S*b*Cn - x_cg*Y_A  # w.r.t. body z-axis
+
+        F_A = np.array([X_A, Y_A, Z_A])  # aerodynamic force [N]
+        M_A = np.array([l_A, m_A, n_A])  # aerodynamic moment [N*m]
+
+        # thruster force and moment are computed here
+        T = Tmax*delt  # thrust [N]
+        X_T, Y_T, Z_T = T, 0, 0  # thruster force body axes component [N]
+        l_T, m_T, n_T = 0, 0, 0  # thruster moment body axes component [N*m]
+
+        # Thruster force, momentum, and gravity force
+        F_T = np.array([X_T, Y_T, Z_T])  # in body coordinate [N]
+        M_T = np.array([l_T, m_T, n_T])  # in body coordinate [N*m]
+        F_G = quat2dcm(q).dot(np.array([0, 0, self.mass*self.g]))
+
+        F = F_A + F_T + F_G
+        M = M_A + M_T
+
+        return F, M
+
+    def get_trim(self, z0={"alpha": 0, "delt": 0, "dele": 0},
+                 fixed={"h": 300, "VT": 16, "eta": (0, 0)}):
+        z0 = list(z0.values())
+        fixed = list(fixed.values())
+        bounds = (
+            (self.coords["alpha"].min(), self.coords["alpha"].max()),
+            self.control_limits["delt"],
+            self.control_limits["dele"]
+        )
+        result = scipy.optimize.minimize(
+            self._trim_cost, z0, args=(fixed,),
+            bounds=bounds, method='SLSQP', options={'disp': True})
+
+        x_trim, u_trim, eta_trim = self._trim_convert(result.x, fixed)
+
+        return x_trim, u_trim, eta_trim
+
+    def _trim_cost(self, z, fixed):
+        x, u, eta = self._trim_convert(z, fixed)
+
+        self.set_dot(x, u, eta)
+        weight = np.diag([1, 1, 1000])
+
+        dxs = np.append(self.vel.dot[(0,2),], self.omega.dot[1])
+        return dxs.dot(weight).dot(dxs)
+
+    def _trim_convert(self, z, fixed):
+        h, VT, eta = fixed
+        alp = z[0]
+        v = np.array([VT*cos(alp), 0, VT*sin(alp)])
+        omega = np.array([0, 0, 0])
+        q = angle2quat(0, alp, 0)
+        p = np.array([0, 0, -h])
+        delt, dele, dela, delr = z[1], z[2], 0, 0
+
+        x = np.hstack((v, omega, q, p))
+        u = np.array([delt, dele, dela, delr])
+        return x, u, eta
+
+
+def rho(altitude):
+    pressure = 101325 * (1 - 2.25569e-5 * altitude)**5.25616
+    temperature = 288.14 - 0.00649 * altitude
+    return pressure / (287*temperature)
---
+++ fym/utils/rot.py
@@ -0,0 +1,59 @@
+import numpy.linalg as nla
+import scipy.linalg as sla
+import numpy as np
+from numpy import sin, cos
+
+
+def quat2dcm(q):
+    q0, q1, q2, q3 = q / nla.norm(q)
+
+    return np.array([
+        [q0**2 + q1**2 - q2**2 - q3**2, 2*(q1*q2 + q0*q3), 2*(q1*q3 - q0*q2)],
+        [2*(q1*q2 - q0*q3), q0**2 - q1**2 + q2**2 - q3**2, 2*(q2*q3 + q0*q1)],
+        [2*(q1*q3 + q0*q2), 2*(q2*q3 - q0*q1), q0**2 - q1**2 - q2**2 + q3**2]
+    ])
+
+
+def angle2quat(yaw, pitch, roll):
+    cy = cos(yaw * 0.5)
+    sy = sin(yaw * 0.5)
+    cr = cos(roll * 0.5)
+    sr = sin(roll * 0.5)
+    cp = cos(pitch * 0.5)
+    sp = sin(pitch * 0.5)
+
+    q = np.zeros(4)
+    q[0] = cy * cr * cp + sy * sr * sp
+    q[1] = cy * sr * cp - sy * cr * sp
+    q[2] = cy * cr * sp + sy * sr * cp
+    q[3] = sy * cr * cp - cy * sr * sp
+    return q
+
+
+def quat2angle(quat):
+    qin = quat / nla.norm(quat)
+
+    r11 = 2 * (qin[1] * qin[2] + qin[0] * qin[3])
+    r12 = qin[0]**2 + qin[1]**2 - qin[2]**2 - qin[3]**2
+    r21 = - 2 * (qin[1] * qin[3] - qin[0] * qin[2])
+    r31 = 2 * (qin[2] * qin[3] + qin[0] * qin[1])
+    r32 = qin[0]**2 - qin[1]**2 - qin[2]**2 + qin[3]**2
+
+    return np.arctan2(r11, r12), np.arcsin(r21), np.arctan2(r31, r32)
+
+
+def angle2dcm(a1, a2, a3):
+    return np.array([
+        [cos(a2) * cos(a1), cos(a2) * sin(a1), - sin(a2)],
+        [- cos(a3) * sin(a1) + sin(a3) * sin(a2) * cos(a1),
+         cos(a3) * cos(a1) + sin(a3) * sin(a2) * sin(a1), sin(a3) * cos(a2)],
+        [sin(a3) * sin(a1) + cos(a3) * sin(a2) * cos(a1),
+         - sin(a3) * cos(a1) + cos(a3) * sin(a2) * sin(a1), cos(a3) * cos(a2)]
+    ])
+
+
+def velocity2polar(vel):
+    norm = sla.norm(vel)
+    chi = np.arctan2(vel[1], vel[0])
+    gamma = np.arcsin(- vel[2] / norm)
+    return np.array([norm, chi, gamma])
