45ccefa32d35209a62ea738e654f853fb92e8da0
Seong-hun Kim <kshoon92@gmail.com>
2021-04-30 20:15:35 +0900

    Add a Multirotor class (#163)

--- fym/models/quadrotor.py
+++ fym/models/quadrotor.py
@@ -1,9 +1,9 @@
 import gym
 from gym import spaces
 import numpy as np
-from scipy.spatial.transform import Rotation as R
 
 from fym.core import BaseEnv, BaseSystem
+from fym.utils import rot
 
 
 class Quadrotor(BaseEnv):
@@ -17,22 +17,22 @@ class Quadrotor(BaseEnv):
         is downward.
 
     Variables:
-        R: SO(3)
+        dcm: SO(3)
             the rotation matrix from the body-fixed frame to the inertial frame
+            dcm = C_{i/b}
     """
     J = np.diag([0.0820, 0.0845, 0.1377])  # kg * m^2
     m = 4.34  # kg
     d = 0.0315  # m
     c = 8.004e-4  # m
     g = 9.81  # m/s^2
-    control_size = 4  # f1, f2, f3, f4
-    name = "quadrotor"
-    state_lower_bound = np.array(-np.inf * np.ones(18))
-    state_upper_bound = np.array(np.inf * np.ones(18))
-    control_lower_bound = np.array(-np.inf * np.ones(4))
-    control_upper_bound = np.array(np.inf * np.ones(4))
 
-    def __init__(self, pos, vel, dcm, omega):
+    e3 = np.vstack((0, 0, 1))
+
+    name = "quadrotor"
+
+    def __init__(self, pos=np.zeros((3, 1)), vel=np.zeros((3, 1)),
+                 dcm=np.eye(3), omega=np.zeros((3, 1))):
         super().__init__()
         self.pos = BaseSystem(pos)
         self.vel = BaseSystem(vel)
@@ -49,11 +49,10 @@ class Quadrotor(BaseEnv):
         ).dot(control)
         M = np.vstack((M1, M2, M3))
 
-        m, g, J = self.m, self.g, self.J
-        e3 = np.vstack((0, 0, 1))
+        m, g, J, e3 = self.m, self.g, self.J, self.e3
 
         dpos = vel
-        dvel = g*e3 - F*dcm.dot(e3)/m
+        dvel = g*e3 - F * dcm.dot(e3)/m
         ddcm = dcm.dot(hat(omega))
         domega = np.linalg.inv(J).dot(M - np.cross(omega, J.dot(omega), axis=0))
         return dpos, dvel, ddcm, domega
@@ -62,6 +61,14 @@ class Quadrotor(BaseEnv):
         pos, vel, dcm, omega = self.observe_list()
         dots = self.deriv(pos, vel, dcm, omega, control)
         self.pos.dot, self.vel.dot, self.dcm.dot, self.omega.dot = dots
+
+    def angle2dcm(self, angle):
+        """angle: psi, theta, phi in radian"""
+        return rot.angle2dcm(*angle)
+
+    def dcm2angle(self, dcm):
+        """angle: psi, theta, phi in radian"""
+        return rot.dcm2anglE(dcm)
 
 
 def hat(v):
@@ -73,11 +80,63 @@ def hat(v):
     ])
 
 
-if __name__ == '__main__':
-    x = np.zeros((3, 1))
-    v = np.zeros((3, 1))
-    dcm = R.from_euler('ZYX', [0, 0, 0]).as_dcm()
-    omega = np.zeros((3, 1))
+class MultiRotor(BaseEnv):
+    """
+    Prof. Taeyoung Lee's model for quadrotor UAV is used.
+    - (https://www.math.ucsd.edu/~mleok/pdf/LeLeMc2010_quadrotor.pdf)
 
-    system = Quadrotor(x, v, dcm, omega)
-    system.set_dot(0, np.zeros((4, 1)))
+    Description:
+        - an NED frame is used for the inertia and body fixed frame.
+        Hence, `+z` direction is downward.
+        - pos and vel are resolved in the inertial frame,
+        whereas dcm and omega are resolved in the body frame
+
+    Variables:
+        dcm: SO(3)
+            the rotation matrix from the body-fixed frame to the inertial frame
+            dcm = C_{i/b}
+    """
+    g = 9.81  # m/s^2
+    e3 = np.vstack((0, 0, 1))
+
+    name = "multirotor"
+
+    def __init__(self,
+                 pos=np.zeros((3, 1)),
+                 vel=np.zeros((3, 1)),
+                 dcm=np.eye(3),
+                 omega=np.zeros((3, 1)),
+                 J=np.diag([0.0820, 0.0845, 0.1377]),
+                 m=4.34,
+                 config="Quadrotor",
+                 ):
+        super().__init__()
+        self.pos = BaseSystem(pos)
+        self.vel = BaseSystem(vel)
+        self.dcm = BaseSystem(dcm)
+        self.omega = BaseSystem(omega)
+
+        self.J = J
+        self.m = m
+
+    def deriv(self, pos, vel, dcm, omega, F, M):
+        m, g, J, e3 = self.m, self.g, self.J, self.e3
+
+        dpos = vel
+        dvel = g * e3 - F * dcm.dot(e3) / m
+        ddcm = dcm.dot(hat(omega))
+        domega = np.linalg.inv(J).dot(M - np.cross(omega, J.dot(omega), axis=0))
+        return dpos, dvel, ddcm, domega
+
+    def set_dot(self, t, F, M):
+        pos, vel, dcm, omega = self.observe_list()
+        dots = self.deriv(pos, vel, dcm, omega, F, M)
+        self.pos.dot, self.vel.dot, self.dcm.dot, self.omega.dot = dots
+
+    def angle2dcm(self, angle):
+        """angle: psi, theta, phi in radian"""
+        return rot.angle2dcm(*angle)
+
+    def dcm2angle(self, dcm):
+        """angle: psi, theta, phi in radian"""
+        return rot.dcm2anglE(dcm)
