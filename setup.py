f04b3a072504f2fd2afc332ceff192343879420b
Seong-hun Kim <kshoon92@gmail.com>
2021-05-03 16:47:24 +0900

    Add some information in setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,20 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='fym',
-    version='1.1.1',
+    version='1.1.3',
     url='https://github.com/fdcl-nrf/fym',
     author='SNU FDCL',
+    author_email='kshoon92@gmail.com',
     description='SNU FDCL Fym: Flight simulator for various purpose',
     long_description=long_description,
     long_description_content_type='text/markdown',
     include_package_data=True,
+    license_files=['LICENSE'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     packages=find_packages(),
     python_requires=">=3.7",
     install_requires=[
