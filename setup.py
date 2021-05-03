8317b97c916500cca19e9443b0b4b5f1071ebedf
Seong-hun Kim <kshoon92@gmail.com>
2021-05-03 16:31:44 +0900

    Modify setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,12 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='fym',
-    version='1.1.0',
+    version='1.1.1',
     url='https://github.com/fdcl-nrf/fym',
     author='SNU FDCL',
     description='SNU FDCL Fym: Flight simulator for various purpose',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    license='',
     include_package_data=True,
     packages=find_packages(),
     python_requires=">=3.7",
