a5c4c3d2a7c46d4dfd17742f61cd9825379fbda5
Seong-hun Kim <kshoon92@gmail.com>
2021-06-17 16:05:55 +0900

    Add logging.save to __init__

--- fym/__init__.py
+++ fym/__init__.py
@@ -1,5 +1,5 @@
 from .core import BaseEnv, BaseSystem
 from .utils import parser
 from .utils.linearization import jacob_analytic, jacob_numerical
-from .logging import load, Logger
+from .logging import load, save, Logger
 from .agents.LQR import clqr, dlqr
