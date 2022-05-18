26687b81ff5c60a670b6cac893570d4e5dd2e2b1
Seong-hun Kim <kshoon92@gmail.com>
2022-05-18 16:28:18 +0900

    Update Logger mode
    ``mode`` now can be one of flush, deque, and step.
    This feature is for Logger acting as a replay buffer.

--- fym/logging.py
+++ fym/logging.py
@@ -1,31 +1,37 @@
+import os
 import pickle
+
 import h5py
 import numpy as np
-import os
-from datetime import datetime
-import fym.parser
 
 
 class Logger:
-    def __init__(self, path=None, log_dir=None, file_name="data.h5",
-                 max_len=1e3, mode="w"):
+    def __init__(self, path=None, max_len=1e3, mode="flush"):
+        assert mode in ["flush", "deque", "stop"]
+
         if path is None:
-            if log_dir is None:
-                log_dir = os.path.join(
-                    'log', datetime.today().strftime('%Y%m%d-%H%M%S'))
-            os.makedirs(log_dir, exist_ok=True)
-            path = os.path.join(log_dir, file_name)
+            assert mode != "flush"
+        else:
+            # To overwite if a file of the same path exists
+            self.path = path
+            with h5py.File(self.path, 'w'):
+                pass
 
-        self.path = path
-        with h5py.File(self.path, mode):
-            pass
-        self.mode = mode
         self.max_len = int(max_len)
+        self.mode = mode
         self._info = {}
-
         self._inner = False
-
         self.clear()
+
+    def len(self):
+        return self._ind + 1
+
+    def __len__(self):
+        return self.len()
+
+    @property
+    def buffer(self):
+        return self._rec_get(self._buf, self._ind)
 
     @property
     def path(self):
@@ -41,24 +47,32 @@ class Logger:
         self._path = path
 
     def clear(self):
-        self.buffer = {}
-        self.index = 0
+        self._buf = {}
+        self._ind = -1
 
-    def record(self, **kwargs):
+    def record(self, **kw):
         assert not self._inner, "Inner loggers are not allowed to record directly"
-        self._record(**kwargs)
+        self._record(**kw)
 
-    def _record(self, **kwargs):
+    def _record(self, **kw):
         """Record a dictionary or a numeric data preserving the structure."""
-        self._rec_update(self.buffer, kwargs)
-        self.index += 1
 
-        if self.index >= self.max_len:
-            self.flush()
+        if self.len() == self.max_len:  # index reaches the last one
+            if self.mode == "flush":
+                self.flush()  # make ``_ind`` 0 and ``_full`` False
+                self._rec_update(self._buf, kw, self._ind + 1)
+                self._ind += 1
+            elif self.mode == "rotate":
+                self._rec_update(self._buf, kw, self._ind, rotate=True)
+            else:  # mode == "stop"
+                pass
+        else:
+            self._rec_update(self._buf, kw, self._ind + 1)
+            self._ind += 1
 
     def flush(self):
         with h5py.File(self.path, "r+") as h5file:
-            _rec_save(h5file, '/', self.buffer, self.index)
+            _rec_save(h5file, '/', self._buf, self._ind)
         self.clear()
 
     def close(self):
@@ -66,24 +80,50 @@ class Logger:
         with h5py.File(self.path, "r+") as h5file:
             _info_save(h5file, self._info)
 
-    def set_info(self, **kwargs):
-        fym.parser.update(self._info, kwargs)
+    def reopen(self):
+        with h5py.File(self.path, "r+"):
+            pass
+
+    def set_info(self, **kw):
+        self._info.update(kw)
         with h5py.File(self.path, "r+") as h5file:
             _info_save(h5file, self._info)
 
-    def _rec_update(self, base_dict, input_dict):
-        """Recursively update ``base_dict`` with ``input_dict``."""
+    def _rec_update(self, base_dict, input_dict, index, rotate=False):
+        """Recursively update ``base_dict`` with ``input_dict``.
+
+        The ``input_dict`` will be added in the ``base_dict[...][index]``.
+        """
         for key, val in input_dict.items():
             if isinstance(val, dict):
                 if key not in base_dict:
                     base_dict[key] = {}
-                self._rec_update(base_dict[key], val)
+                self._rec_update(base_dict[key], val, index)
             elif not isinstance(val, str):
+                val = np.asarray(val)
                 if key not in base_dict:
-                    base_dict[key] = np.empty((self.max_len,) + np.shape(val))
-                base_dict[key][self.index] = np.copy(val)
+                    base_dict[key] = np.empty(
+                        (self.max_len,) + val.shape,
+                        dtype=val.dtype,
+                    )
+
+                if rotate:
+                    base_dict[key][:-1] = base_dict[key][1:]
+
+                base_dict[key][index] = np.copy(val)
             else:
                 raise ValueError("Unsupported data type")
+
+    def _rec_get(self, base_dict, index):
+        out_dict = {}
+        for key, val in base_dict.items():
+            if isinstance(val, dict):
+                out_dict[key] = self._rec_get(val, index)
+            elif not isinstance(val, str):
+                out_dict[key] = val[:index + 1]
+            else:
+                raise ValueError("Unsupported data type")
+        return out_dict
 
 
 def save(h5file, dic, mode="w", info=None):
@@ -117,13 +157,13 @@ def _rec_save(h5file, path, dic, index=None):
             if isinstance(val, list):
                 val = np.stack(val)
             if index is not None:
-                val = val[:index]
+                val = val[:index + 1]
             if path + key not in h5file:
                 dset = h5file.create_dataset(
                     path + key,
                     shape=val.shape,
                     maxshape=(None,) + val.shape[1:],
-                    dtype=float,
+                    dtype=val.dtype,
                 )
                 dset[:] = val
             else:
