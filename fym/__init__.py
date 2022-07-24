42544773dc2d86cf38bec60503d72d3f7660d8be
Seong-hun Kim <kshoon92@gmail.com>
2022-07-24 22:54:19 +0900

    Add ray tune generate variants method

--- fym/__init__.py
+++ fym/__init__.py
@@ -4,3 +4,4 @@ from .agents.LQR import clqr, dlqr
 from .core import BaseEnv, BaseSystem, Sequential
 from .logging import Logger, load, save
 from .utils.linearization import jacob_analytic, jacob_numerical
+from .utils.ray import generate_variants
